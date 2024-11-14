957e4d58a132b556ba6502c9c1b1db66dab73249
Hwang TaeYeon <ty5321@naver.com>
2024-11-14 20:22:58 +0900

    123

---
+++ src/Card/ThiefCardFactory.java
@@ -0,0 +1,27 @@
+package Card;
+
+import Card.Card;
+import Card.CardFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThiefCardFactory implements CardFactory {
+    @Override
+    public List<Card> createCards() {
+        List<Card> cards = new ArrayList<>();
+        String[] suits = { "♤", "♡", "◇", "♧"};
+        String[] names = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        int[] values = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
+        Card joker=new PlayCard("joker",0);
+
+        for (String suit : suits) {
+            for (int i = 0; i < names.length; i++) {
+                cards.add(new PlayCard(  suit+names[i], values[i]));
+            }
+            cards.add(joker);
+        }
+        return cards;
+    }
+
+}
